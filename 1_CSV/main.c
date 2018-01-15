efc078d02598d7875683de049a6535d4b6ee7b58
Cédric Thomas <cedric.thomas@protonmail.com>
2018-01-15 15:24:03 +0100

    CSV Final - Modif

--- 1_CSV/main.c
+++ 1_CSV/main.c
@@ -3,15 +3,26 @@
 #include "balle.h"
 #include "vecteur.h"
 
-int main(void)
+#define FILENAME_IMPORT "balle.txt"
+#define FILENAME_EXPORT "export.csv"
+
+void gnrt_csv(int temps_sec, float pas_sec)
 {
-	Balle b = chargerBalle("config.txt");
+	Balle b = chargerBalle(FILENAME_IMPORT);
+	
+	FILE* csv = fopen(FILENAME_EXPORT, "w");
+	
 	float i;
-	FILE* csv = fopen("export.csv", "w");
-	for(i = 0; i < 3; i += 0.01)
+	
+	for(i = 0; i < temps_sec; i += pas_sec)
 	{
 		 fprintf(csv, "%f;%f;%f\n", i, b.position.x, b.position.y);
 		 majPosition(&b, 0.01);
 	}
 	fclose(csv);
+}
+
+int main(void)
+{
+	gnrt_csv(3, 0.01);
 }
